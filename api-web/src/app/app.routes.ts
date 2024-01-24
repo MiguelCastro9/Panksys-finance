8647fd3b9e20f06408ba9149735793b491c821a3
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-01-24 18:37:50 -0300

    settings in components and routers

--- api-web/src/app/app.routes.ts
+++ api-web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Error404Component } from './errors/error-404/error-404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: Error404Component}
+];
