f746e81b82a087b5d9eb1445fb2142a5b5941fb9
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-01-25 20:32:24 -0300

    settings

--- api-web/src/app/dashboard/dashboard.component.ts
+++ api-web/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  constructor() {
+    Chart.register(...registerables);
+   }
+
+   @ViewChild("total_finances", {static: true}) element_total_finances!: ElementRef;
+   @ViewChild("monthly_finances", {static: true}) element_monthly_finances!: ElementRef;
+
+   ngOnInit(): void {
+
+    new Chart(this.element_total_finances.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ["Comédia","Romance","Ação","Terror", "Drama"],
+        datasets: [{
+          data: [10, 20, 30, 40, 50]
+        }]
+      },
+      options: {
+        responsive: true
+      }
+    });
+
+    new Chart(this.element_monthly_finances.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ["Comédia","Romance","Ação","Terror", "Drama"],
+        datasets: [{
+          data: [10, 20, 30, 40, 50]
+        }]
+      },
+      options: {
+        responsive: true
+      }
+    });
+   }
 }
