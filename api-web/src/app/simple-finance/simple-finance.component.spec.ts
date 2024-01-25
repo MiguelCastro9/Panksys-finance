d49debae0295c0adabaef5ae6dbd71ce531a086c
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-01-25 12:28:38 -0300

    settings

--- api-web/src/app/simple-finance/simple-finance.component.spec.ts
+++ api-web/src/app/simple-finance/simple-finance.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SimpleFinanceComponent } from './simple-finance.component';
 
 describe('SimpleFinanceComponent', () => {
@@ -11,7 +10,7 @@ describe('SimpleFinanceComponent', () => {
       imports: [SimpleFinanceComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(SimpleFinanceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
