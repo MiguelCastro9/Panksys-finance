75520814a04ee26b2e956ce1fcd22a0fa1e4cacf
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-01-28 11:06:11 -0300

    create component sign-in

--- api-web/src/app/app.routes.ts
+++ api-web/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Error404Component } from './errors/error-404/error-404.component';
 import { SimpleFinanceComponent } from './simple-finance/simple-finance.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'simple-finance', component: SimpleFinanceComponent },
   { path: 'feedback', component: FeedbackComponent },
