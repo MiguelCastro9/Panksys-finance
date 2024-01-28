2b259ad9fc3900997a0d47deb7928d962390e0f3
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-01-27 21:41:21 -0300

    create component at feedback

--- api-web/src/app/app.routes.ts
+++ api-web/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Error404Component } from './errors/error-404/error-404.component';
 import { SimpleFinanceComponent } from './simple-finance/simple-finance.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'simple-finance', component: SimpleFinanceComponent },
+  { path: 'feedback', component: FeedbackComponent },
   { path: '**', component: Error404Component }
 ];
---
+++ api-web/src/app/feedback/feedback.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-feedback',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.css'
+})
+export class FeedbackComponent {
+
+}
