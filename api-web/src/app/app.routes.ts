dd673f138304ef93fac459994c9723882290eb83
MiguelCastro9 <miguel.castro9@hotmail.com>
2024-01-29 21:26:56 -0300

    create componente sign-up

--- api-web/src/app/app.routes.ts
+++ api-web/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Error404Component } from './errors/error-404/error-404.component';
 import { SimpleFinanceComponent } from './simple-finance/simple-finance.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'sign-up', component: SignUpComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'simple-finance', component: SimpleFinanceComponent },
